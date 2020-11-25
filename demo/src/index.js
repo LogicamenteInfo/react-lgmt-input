abd9b0de74a4188335e677c07a67566da16fdb01
Natanael Simões <natanael.simoes@logicamente.info>
2020-11-24 23:47:54 -0400

    ref support

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,64 +1,81 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import LgmtInput from '../../src'
+import LgmtInput from "../../src";
 
 export default class Demo extends Component {
   state = {
-    moeda: '0,00'
+    moeda: "0,00",
+  };
+
+  componentDidMount() {
+    this.inputAgencia.inputRef.focus();
   }
 
   render() {
-    return <div>
-      <h1>react-lgmt-input Demo</h1>
+    return (
+      <div>
+        <h1>react-lgmt-input Demo</h1>
 
-      <form>
-        <label><b>Agência</b></label>
-        <LgmtInput.AgenciaBancaria
-          className="class--test"
-          value={this.state.agencia}
-          onChange={(agencia) => this.setState({ agencia })}
-        />
-        <p>Você digitou o agência: {this.state.agencia}</p>
-      </form>
+        <form>
+          <label>
+            <b>Agência</b>
+          </label>
+          <LgmtInput.AgenciaBancaria
+            ref={(e) => (this.inputAgencia = e)}
+            className="class--test"
+            value={this.state.agencia}
+            onChange={(agencia) => this.setState({ agencia })}
+          />
+          <p>Você digitou o agência: {this.state.agencia}</p>
+        </form>
 
-      <form>
-        <label><b>Conta</b></label>
-        <LgmtInput.ContaBancaria
-          value={this.state.conta}
-          onChange={(conta) => this.setState({ conta })}
-        />
-        <p>Você digitou a conta: {this.state.conta}</p>
-      </form>
+        <form>
+          <label>
+            <b>Conta</b>
+          </label>
+          <LgmtInput.ContaBancaria
+            value={this.state.conta}
+            onChange={(conta) => this.setState({ conta })}
+          />
+          <p>Você digitou a conta: {this.state.conta}</p>
+        </form>
 
-      <form>
-        <label><b>CPF</b></label>
-        <LgmtInput.CPF
-          value={this.state.cpf}
-          onChange={(cpf) => this.setState({ cpf })}
-        />
-        <p>Você digitou o CPF: {this.state.cpf}</p>
-      </form>
+        <form>
+          <label>
+            <b>CPF</b>
+          </label>
+          <LgmtInput.CPF
+            value={this.state.cpf}
+            onChange={(cpf) => this.setState({ cpf })}
+          />
+          <p>Você digitou o CPF: {this.state.cpf}</p>
+        </form>
 
-      <form>
-        <label><b>Documento</b></label>
-        <LgmtInput.Documento
-          value={this.state.documento}
-          onChange={(documento) => this.setState({ documento })}
-        />
-        <p>Você digitou o documento: {this.state.documento}</p>
-      </form>
+        <form>
+          <label>
+            <b>Documento</b>
+          </label>
+          <LgmtInput.Documento
+            value={this.state.documento}
+            onChange={(documento) => this.setState({ documento })}
+          />
+          <p>Você digitou o documento: {this.state.documento}</p>
+        </form>
 
-      <form>
-        <label><b>Moeda</b></label>
-        <LgmtInput.Moeda
-          value={this.state.moeda}
-          onChange={(moeda) => this.setState({ moeda })}
-        />
-        <p>Você digitou o valor : R$ {this.state.moeda}</p>
-      </form>
-    </div>
+        <form>
+          <label>
+            <b>Moeda</b>
+          </label>
+          <LgmtInput.Moeda
+            value={this.state.moeda}
+            onChange={(moeda) => this.setState({ moeda })}
+          />
+          <p>Você digitou o valor : R$ {this.state.moeda}</p>
+        </form>
+      </div>
+    );
   }
 }
 
-render(<Demo />, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/Components/_CommonInput.js
+++ src/Components/_CommonInput.js
@@ -1,43 +1,48 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class CommonInput extends Component {
   static propTypes = {
     value: PropTypes.any,
-    onChange: PropTypes.func
-  }
+    onChange: PropTypes.func,
+  };
 
   static defaultProps = {
-    value: '',
-    onChange: () => { }
-  }
+    value: "",
+    onChange: () => {},
+  };
 
   constructor(props) {
     super(props);
     this.state = {
-      value: this.props.value.toString()
-    }
+      value: this.props.value.toString(),
+    };
   }
 
   componentDidMount() {
     if (this.masksRegex === undefined)
       throw "O atributo `masksRegex` é obrigatório.";
-    if (typeof this.masksRegex !== 'object' || this.masksRegex.constructor.name !== 'RegExp')
+    if (
+      typeof this.masksRegex !== "object" ||
+      this.masksRegex.constructor.name !== "RegExp"
+    )
       throw "O atributo `masksRegex` precisa ser uma expressão regular.";
     if (this.masksReplacement === undefined)
       throw "O atributo `masksReplacement` é obrigatório.";
     if (!Array.isArray(this.masksReplacement))
       throw "O atributo `masksReplacement` precisa ser um array.";
-    if (this.length === undefined)
-      throw "O atributo `length` é obrigatório.";
-    if (typeof this.length !== 'number')
+    if (this.length === undefined) throw "O atributo `length` é obrigatório.";
+    if (typeof this.length !== "number")
       throw "O atributo `length` precisa ser um número inteiro.";
-    if (this.pattern
-      && (typeof this.pattern !== 'object' || this.pattern.constructor.name !== 'RegExp'))
+    if (
+      this.pattern &&
+      (typeof this.pattern !== "object" ||
+        this.pattern.constructor.name !== "RegExp")
+    )
       throw "O atributo `pattern` precisa ser uma expressão regular.";
-    if (this.invalidMessage && typeof this.invalidMessage !== 'string')
+    if (this.invalidMessage && typeof this.invalidMessage !== "string")
       throw "O atributo `invalidMessage` precisa ser um string.";
-    if (this.validator && typeof this.validator !== 'function')
+    if (this.validator && typeof this.validator !== "function")
       throw "O atributo `validator` precisa ser uma função.";
   }
 
@@ -62,31 +67,54 @@ export default class CommonInput extends Component {
         if (rValue.replace(/\D/g, "").length > 0) break;
       }
       const nonDigits = rValue.match(/(\D| )/g);
-      const offset = nonDigits !== null && end === previousValue.length ? nonDigits.length : 0;
-      this.setState({ value: rValue }, ((el) => {
-        this.props.onChange(this.state.value);
-        el.setSelectionRange(start + offset, end + offset);
-        el.setCustomValidity("");
-        if (this.state.value.toString().length > 0 && typeof this.validator === 'function' && !this.validator()) {
-          el.setCustomValidity(this.invalidMessage || 'O valor informado não é válido');
-        }
-      }).bind(this, event.target));
+      const offset =
+        nonDigits !== null && end === previousValue.length
+          ? nonDigits.length
+          : 0;
+      this.setState(
+        { value: rValue },
+        ((el) => {
+          this.props.onChange(this.state.value);
+          el.setSelectionRange(start + offset, end + offset);
+          el.setCustomValidity("");
+          if (
+            this.state.value.toString().length > 0 &&
+            typeof this.validator === "function" &&
+            !this.validator()
+          ) {
+            el.setCustomValidity(
+              this.invalidMessage || "O valor informado não é válido"
+            );
+          }
+        }).bind(this, event.target)
+      );
     }
   }
 
   onInvalid(event) {
     if (this.invalidMessage && !event.target.validity.valid) {
-      event.target.setCustomValidity(this.invalidMessage || 'O valor informado não é válido');
+      event.target.setCustomValidity(
+        this.invalidMessage || "O valor informado não é válido"
+      );
     }
   }
 
   render() {
-    return React.createElement('input', {
-      ...this.props,
-      value: this.state.value,
-      onChange: (e) => this.onChange(e),
-      pattern: this.pattern?.toString().substr(1, this.pattern.toString().length - 2) ?? '.*',
-      onInvalid: (e) => this.onInvalid(e)
-    });
+    return (
+      <input
+        {...this.props}
+        ref={(e) => {
+          this.inputRef = e;
+        }}
+        value={this.state.value}
+        onChange={(e) => this.onChange(e)}
+        onInvalid={(e) => this.onInvalid(e)}
+        pattern={
+          this.pattern
+            ?.toString()
+            .substr(1, this.pattern.toString().length - 2) ?? ".*"
+        }
+      />
+    );
   }
 }
